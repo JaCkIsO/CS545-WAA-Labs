2133697a58776912e98b899bb52ced9a919d4131
JaCkIsO <jackisodev@gmail.com>
2021-04-26 14:35:43 -0500

    Update PostServiceImpl.java
    Add Transactional Annotation on Service

--- Lab3/src/main/java/edu/miu/service/impl/PostServiceImpl.java
+++ Lab3/src/main/java/edu/miu/service/impl/PostServiceImpl.java
@@ -6,11 +6,13 @@ import edu.miu.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
+@Transactional
 public class PostServiceImpl implements PostService {
 
     @Autowired
