dc50a2a165e35571f579176f7c00cca758280d9b
JaCkIsO <jackisodev@gmail.com>
2021-04-26 14:17:59 -0500

    Update Post.java
    Refactor Domain

--- Lab3/src/main/java/edu/miu/domain/Post.java
+++ Lab3/src/main/java/edu/miu/domain/Post.java
@@ -2,14 +2,22 @@ package edu.miu.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
+    @Column(name = "TITLE")
     String title;
+    @Column(name = "CONTENT")
     String content;
+    @Column(name = "AUTHOR")
     String author;
 }
